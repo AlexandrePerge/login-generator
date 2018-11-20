f52c7ed8fd43ef38e073ba0bb84309b368e76bc2
Alexandre Perge <alexandre.perge@univ-tlse3.fr>
2018-11-20 17:00:13 +0100

    #1 test loginService

--- src/test/java/geco/LoginServiceTest.java
+++ src/test/java/geco/LoginServiceTest.java
@@ -4,6 +4,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class LoginServiceTest {
@@ -18,17 +21,42 @@ public class LoginServiceTest {
 
     @Test
     public void loginExists() {
+        String[] loginsExistants ={"Paul Dupond"};
+
+        LoginService ls = new LoginService(loginsExistants);
+        assertTrue("le login est reconnue", ls.loginExists("Paul Dupond"));
     }
 
     @Test
     public void addLogin() {
+
+        String[] loginsExistants ={"Paul Dupond"};
+
+        LoginService ls = new LoginService(loginsExistants);
+        ls.addLogin("Pierre Dupard");
+        assertTrue("le login a été ajouté", ls.loginExists("Pierre Dupard"));
+
     }
 
     @Test
     public void findAllLoginsStartingWith() {
+
+        String[] loginsExistants ={"Paul Dupond"};
+
+        LoginService ls = new LoginService(loginsExistants);
+        //   ls.addLogin("Pierre Dupard");
+        assertEquals("Paul Dupond", ls.findAllLoginsStartingWith("Paul").get(0));
     }
 
     @Test
     public void findAllLogins() {
+        String[] loginsExistants ={"Paul Dupond"};
+        List<String> lss = new ArrayList<>();
+        lss.add("Paul Dupond");
+        lss.add("Pierre Dupard");
+
+        LoginService ls = new LoginService(loginsExistants);
+        ls.addLogin("Pierre Dupard");
+        assertEquals(lss, ls.findAllLogins());
     }
 }
